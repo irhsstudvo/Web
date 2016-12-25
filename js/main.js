2f88bda0ee82f4896b8c56d0774d2bd79c5536f1
IbrahimIrfan <1ibrahimirfan@gmail.com>
2016-12-25 17:15:54 -0500

    eregistration form, etc

--- js/main.js
+++ js/main.js
@@ -10,15 +10,43 @@ $(document).on("ready", function() {
         }, 1000);
     }
 
-        // fade an image out and fade in a new image
-        function fadeImage(item, delay, imgPath) {
-            item.fadeOut(delay, function() {
-                item.attr('src', imgPath);
-                item.fadeIn(delay);
-            });
-        }
-        
-    // green arrow on hover, else white
+    // fade an image out and fade in a new image
+    function fadeImage(item, delay, imgPath) {
+        item.fadeOut(delay, function() {
+            item.attr('src', imgPath);
+            item.fadeIn(delay);
+        });
+    }
+
+    // move slightly up while fading into view
+    function fadeInAnimation(elem) {
+        elem.css("top", "+=30px");
+        elem.animate({
+            'opacity': '1',
+            'top': '-=30px'
+        }, 1000);
+    }
+
+    function checkFadeIn(){
+      $('.fadeIn').each(function(i) {
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+          if (bottom_of_window > bottom_of_object && $(this).css("opacity") == "0") {
+              fadeInAnimation($(this));
+              $(this).removeClass("fadeIn");
+          }
+      });
+    }
+
+    checkFadeIn();
+    // on window scroll, fade item if in view
+    $(window).scroll(function() {
+        checkFadeIn();
+    });
+
+
+    // red arrow on hover, else white
     $('#downArrow').hover(function() {
         fadeImage($(this), 150, 'img/icons/redArrow.png');
     }, function() {
